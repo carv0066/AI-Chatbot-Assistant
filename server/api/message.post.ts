ad0be43960f490b8e2ce02dc6cd121e9d12443c9
Carlos Carvajal <carv0066@algonquinlive.com>
2024-10-28 01:49:25 -0400

    workflow changes

--- server/api/message.post.ts
+++ server/api/message.post.ts
@@ -1,14 +1,19 @@
 import { client, assistant } from "../utilities/openai";
 import { getLatestMessages } from "../utilities/get-message";
+import { getCookie, getQuery } from 'h3';
 
 export default defineEventHandler(async (event) => {
     const threadID = getCookie(event, "thread-id");
 
-    if(!threadID) {
+    if (!threadID) {
         return;
     }
 
     const queryParams = getQuery(event);
+
+    if (!assistant || typeof assistant !== 'string') {
+        throw new Error("Assistant ID is not defined or is invalid.");
+    }
 
     await client.beta.threads.messages.create(threadID, {
         role: "user",
@@ -16,9 +21,8 @@ export default defineEventHandler(async (event) => {
     });
 
     const run = await client.beta.threads.runs.create(threadID, {
-        assistant_id: assistant,
-
+        assistant_id: assistant, 
     });
 
     return await getLatestMessages(threadID, run.id);
-})
+});
